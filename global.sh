973883aa739a7aae588e08dce11d98515d68e80d
Nasty Shade <nastyshade@users.noreply.github.com>
2016-07-23 16:53:30 +0200

    [AUTOINSTALL] Ajout de la configuration globale

---
+++ global.sh
@@ -0,0 +1,16 @@
+source "../spinner.sh"
+
+NEUTRE="\033[0;0m"
+ROUGE="\033[31;1m"
+JAUNE="\033[33;1m"
+RAPPORT="/var/log/AutoInstall.log"
+
+touch ${RAPPORT}
+clear
+
+# SQUID
+PLAGE_IP="192.168.0.0/24"
+MAIL="login@provider.com"
+
+# SARG
+SARG_WWW="/var/www/html/sarg/"
