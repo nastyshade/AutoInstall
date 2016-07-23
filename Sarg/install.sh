d9dd90313e38a3716f90a1c92ef2225ba41ebc53
Nasty Shade <nastyshade@users.noreply.github.com>
2016-07-23 19:52:13 +0200

    [SARG] Correction du script d'installation

--- Sarg/install.sh
+++ Sarg/install.sh
@@ -15,11 +15,21 @@ stop_spinner $?
 echo "# Configuration de Sarg" > ${RAPPORT} 2>&1
 start_spinner "# Configuration de Sarg"
 mkdir ${SARG_WWW} >> ${RAPPORT} 2>&1
-sed -i 's/access_log /var/log/squid/access.log/access_log /var/log/squid3/access.log/' /etc/sarg/sarg.conf >> ${RAPPORT} 2>&1
+cp /etc/sarg/sarg.conf /etc/sarg/sarg.conf.bak >> ${RAPPORT} 2>&1
+cp /etc/sarg/sarg-reports.conf /etc/sarg/sarg-reports.conf.bak >> ${RAPPORT} 2>&1
+sed -i 's/access_log \/var\/log\/squid\/access.log\/access_log/var\/log\/squid3\/access.log/' /etc/sarg/sarg.conf >>$
 sed -i 's/#graphs yes/graphs yes/' /etc/sarg/sarg.conf >> ${RAPPORT} 2>&1
-sed -i 's/#graph_days_bytes_bar_color orange/graph_days_bytes_bar_color orange/' /etc/sarg/sarg.conf >> ${RAPPORT} 2>&1
+sed -i 's/#graph_days_bytes_bar_color orange/graph_days_bytes_bar_color orange/' /etc/sarg/sarg.conf >> ${RAPPORT} 2$
 sed -i 's/charset Latin1/charset UTF-8/' /etc/sarg/sarg.conf >> ${RAPPORT} 2>&1
-sed -i 's/HTMLOUT=/var/lib/sarg/HTMLOUT=${SARG_WWW}' /etc/sarg/sarg-report.conf >> ${RAPPORT} 2>&1
+sed -i "s/HTMLOUT=\/var\/lib\/sarg/HTMLOUT=${SARG_WWW//\//\\/}/" /etc/sarg/sarg-reports.conf >> ${RAPPORT} 2>&1
+stop_spinner $?
+
+echo "# Ajout des cron jobs" > ${RAPPORT} 2>&1
+start_spinner "# Ajout des cron jobs"
+echo "00 08-18/1 * * * root sarg-reports today" >> /etc/crontab
+echo "00 00 * * * root sarg-reports daily" >> /etc/crontab
+echo "00 01 * * 1 root sarg-reports weekly" >> /etc/crontab
+echo "30 02 1 * * root sarg-reports monthly" >> /etc/crontab
 stop_spinner $?
 
 echo ""
